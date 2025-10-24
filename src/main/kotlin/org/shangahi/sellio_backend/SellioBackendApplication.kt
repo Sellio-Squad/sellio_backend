a023fb89ab5dac02f75cc78395fbfe89425166bc
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-24 17:49:19 +0300

    Initial commit

---
+++ src/main/kotlin/org/shangahi/sellio_backend/SellioBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.shangahi.sellio_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SellioBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SellioBackendApplication>(*args)
+}
---
+++ src/test/kotlin/org/shangahi/sellio_backend/SellioBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.shangahi.sellio_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SellioBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
