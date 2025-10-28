ac98b73f8e20f77f21b713e87b014c01e642083d
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-28 14:57:14 +0300

    feat: add color table

---
+++ src/main/kotlin/org/shangahi/sellio_backend/entity/Color.kt
@@ -0,0 +1,19 @@
+package org.shangahi.sellio_backend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Table(name = "color", schema = "sellio_db")
+@Entity
+data class Color(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID = UUID.randomUUID(),
+    @Column(name = "value", nullable = false)
+    val value: String
+)
