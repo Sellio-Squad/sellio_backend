aa438541274895b4c9dbb346ca218a1975b90ebc
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-28 09:07:18 +0300

    feat: add product table

---
+++ src/main/kotlin/org/shangahi/sellio_backend/entity/Product.kt
@@ -0,0 +1,27 @@
+package org.shangahi.sellio_backend.entity
+
+import jakarta.persistence.*
+import java.time.Instant
+import java.util.*
+
+@Table(name = "product", schema = "sellio_db")
+@Entity
+data class Product(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID = UUID.randomUUID(),
+    @Column(name = "title")
+    val title: String,
+    @Column(name = "description", nullable = true)
+    val description: String?,
+    @Column(name = "store_id")
+    val storeId: UUID,
+    @Column(name = "sub_category_id")
+    val subCategoryId: UUID,
+    @Column(name = "is_used")
+    val isUsed: Boolean = false,
+    @Column(name = "price")
+    val price: Double,
+    @Column(name = "created_at")
+    val createdAt: Instant = Instant.now()
+)
