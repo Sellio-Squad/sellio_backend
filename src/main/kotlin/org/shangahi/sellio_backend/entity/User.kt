7e9bcc272e71c691812dd87788679d2385f02dba
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2025-10-28 19:58:41 +0300

    refactor:change table name for user entity

--- src/main/kotlin/org/shangahi/sellio_backend/entity/User.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/entity/User.kt
@@ -1,14 +1,10 @@
 package org.shangahi.sellio_backend.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-import java.util.UUID
+import jakarta.persistence.*
+import java.util.*
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
