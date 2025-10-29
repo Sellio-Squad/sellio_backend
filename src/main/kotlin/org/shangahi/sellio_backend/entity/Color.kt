e26b6196fa81d12caaac7d7a2f3bdbe3153c5b2a
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-10-29 15:09:17 +0300

    feat: add discount table relations

--- src/main/kotlin/org/shangahi/sellio_backend/entity/Color.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/entity/Color.kt
@@ -1,19 +1,15 @@
 package org.shangahi.sellio_backend.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-import java.util.UUID
+import jakarta.persistence.*
+import java.util.*
 
 @Table(name = "color")
 @Entity
 data class Color(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    val id: UUID? = null ,
+    val id: UUID? = null,
+
     @Column(name = "value", nullable = false)
     val value: String
 )
