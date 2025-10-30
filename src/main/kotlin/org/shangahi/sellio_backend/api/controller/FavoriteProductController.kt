7f3fe2b0d7409ffca226a4a82da3f76aff215fa3
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-10-30 23:28:16 +0200

    feat: add endpoint example

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteProductController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteProductController.kt
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
+// http://localhost:8080/v1/favorite-products/userId
 @RestController
 @RequestMapping("/v1/favorite-products")
 class FavoriteProductController(private val favoriteProductService: FavoriteProductService) {
+
     @GetMapping("/{userId}")
     fun findByUserId(@PathVariable userId: UUID): List<FavoriteProductsResponse> {
+
         return favoriteProductService.getFavoriteProductsByUserId(userId)
     }
 }
