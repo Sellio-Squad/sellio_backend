d49b60a5369074b5bb288f6bc1225f4c9f65adbc
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-31 00:24:16 +0300

    feat: add user controller

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/UserInfoController.kt
@@ -0,0 +1,26 @@
+package org.shangahi.sellio_backend.api.controller
+
+import org.shangahi.sellio_backend.api.dto.UserInfoResponse
+import org.shangahi.sellio_backend.api.mapper.toResponse
+import org.shangahi.sellio_backend.service.UserService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/user-info")
+class UserInfoController(
+    private val userService: UserService,
+    @param:Value("spring.datasource.url") private val cdnEndpoint: String,
+    ) {
+
+    @GetMapping
+    fun getUserProfile(@RequestParam userId: UUID): ResponseEntity<UserInfoResponse> {
+        val response = userService.findById(userId).toResponse(cdnEndpoint)
+        return ResponseEntity.ok(response)
+    }
+}
