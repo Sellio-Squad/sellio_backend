fcce2222f367d1bf1f0a63705a708d47424f32ee
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-10-30 21:18:28 +0300

    feat: add Product controller

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/ProductController.kt
@@ -0,0 +1,24 @@
+package org.shangahi.sellio_backend.api.controller
+
+import org.shangahi.sellio_backend.api.dto.ProductCardResponse
+import org.shangahi.sellio_backend.service.ProductService
+import org.springframework.data.domain.Page
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+// GET http://localhost:8080/v1/stores/store_id/products
+@RestController
+@RequestMapping("/v1/stores/{storeId}/products")
+class ProductController(
+    private val productService: ProductService
+) {
+
+    @GetMapping
+    fun getAllProductsForStore(
+        @PathVariable storeId: UUID,
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "20") size: Int
+    ): Page<ProductCardResponse> {
+        return productService.getStoreProducts(storeId, page, size)
+    }
+}
