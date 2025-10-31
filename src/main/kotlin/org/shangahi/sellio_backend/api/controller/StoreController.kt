ebb37723a7b4664ce66af3e940ef38d1e4cc8e5a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-10-31 18:41:00 +0300

    feat: enhance store controller

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/StoreController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/StoreController.kt
@@ -4,8 +4,7 @@ import org.shangahi.sellio_backend.api.dto.PageResponse
 import org.shangahi.sellio_backend.api.dto.StoreResponse
 import org.shangahi.sellio_backend.api.mapper.toResponse
 import org.shangahi.sellio_backend.service.StoreService
-import org.springframework.data.domain.Pageable
-import org.springframework.http.ResponseEntity
+import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -16,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController
 class StoreController(
     private val storeService: StoreService
 ) {
-    @GetMapping("/top-store")
-    fun getTopStores(@RequestParam(defaultValue = "10") pageable: Pageable):
-            ResponseEntity<PageResponse<StoreResponse>> {
-        val page = storeService.getPagedTopStores(pageable)
-        return ResponseEntity.ok(page.toResponse())
+    @GetMapping("/top")
+    fun getTopStores(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "10") size: Int
+    ): PageResponse<StoreResponse> {
+        val pageable = PageRequest.of(page, size)
+        val storesPage = storeService.getPagedTopStores(pageable)
+        return storesPage.toResponse()
     }
 }
+
 
