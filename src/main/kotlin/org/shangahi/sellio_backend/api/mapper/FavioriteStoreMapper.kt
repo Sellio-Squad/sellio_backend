b9663c2887f5554e4036a34e5173a0bcb4bc7b2a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-01 15:53:07 +0300

    feat: add FavoriteStoreMapper

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/mapper/FavioriteStoreMapper.kt
@@ -0,0 +1,25 @@
+package org.shangahi.sellio_backend.api.mapper
+
+import org.shangahi.sellio_backend.api.dto.FavoriteStoreResponse
+import org.shangahi.sellio_backend.api.dto.PageResponse
+import org.shangahi.sellio_backend.entity.FavoriteStore
+import org.springframework.data.domain.Page
+
+fun FavoriteStore.toResponse(): FavoriteStoreResponse {
+    return FavoriteStoreResponse(
+        id = id,
+        storeId = store,
+        userId = user,
+        createdAt = createdAt
+    )
+}
+
+fun Page<FavoriteStore>.toPageResponse(): PageResponse<FavoriteStoreResponse> {
+    return PageResponse(
+        data = content.map { it.toResponse() },
+        totalElements = totalElements,
+        page = number,
+        pageSize = size,
+        totalPages = totalPages
+    )
+}
