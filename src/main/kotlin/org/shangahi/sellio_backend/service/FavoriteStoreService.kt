f0f86efe8c93b26fd2810e14a7c6e1f5afde4822
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-01 15:52:02 +0300

    feat: add FavoriteStoreService

---
+++ src/main/kotlin/org/shangahi/sellio_backend/service/FavoriteStoreService.kt
@@ -0,0 +1,16 @@
+package org.shangahi.sellio_backend.service
+
+import org.shangahi.sellio_backend.entity.FavoriteStore
+import org.shangahi.sellio_backend.repository.FavoriteStoreRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class FavoriteStoreService(private val favoriteStoreRepository: FavoriteStoreRepository) {
+
+    fun getFavoriteStoresByUserId(userId: UUID,pageable: Pageable): Page<FavoriteStore> {
+        return favoriteStoreRepository.findByUserId(userId, pageable)
+    }
+}
