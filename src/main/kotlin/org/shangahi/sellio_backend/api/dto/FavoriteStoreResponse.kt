4ba68fe1755a4f393dcfe4ad5611ca676f49804b
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-01 15:52:42 +0300

    feat: add FavoriteStoreResponse

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/dto/FavoriteStoreResponse.kt
@@ -0,0 +1,13 @@
+package org.shangahi.sellio_backend.api.dto
+
+import org.shangahi.sellio_backend.entity.Store
+import org.shangahi.sellio_backend.entity.User
+import java.time.Instant
+import java.util.*
+
+data class FavoriteStoreResponse(
+    val id: UUID?,
+    val storeId: Store,
+    val userId: User,
+    val createdAt: Instant?
+)
