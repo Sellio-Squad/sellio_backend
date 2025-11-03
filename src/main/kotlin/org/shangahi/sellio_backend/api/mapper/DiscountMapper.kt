5d921a7d7164eb52f236ae2a7f4683937d63edc3
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-03 16:26:31 +0200

    feat: handle mapper error with custom exception

--- src/main/kotlin/org/shangahi/sellio_backend/api/mapper/DiscountMapper.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/mapper/DiscountMapper.kt
@@ -2,10 +2,11 @@ package org.shangahi.sellio_backend.api.mapper
 
 import org.shangahi.sellio_backend.api.dto.DiscountResponse
 import org.shangahi.sellio_backend.entity.Discount
+import org.shangahi.sellio_backend.service.exception.InternalServerErrorException
 
 fun Discount.toDiscountResponse(): DiscountResponse {
     return DiscountResponse(
-        id = this.id?: throw NullPointerException("Discount id is null"),
+        id = this.id?: throw InternalServerErrorException("Discount ID was null during mapping for entity."),
         storeId = this.store?.id,
         productId = this.product?.id,
         subCategoryId = this.subCategory?.id,
