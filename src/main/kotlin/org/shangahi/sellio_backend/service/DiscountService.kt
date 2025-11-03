b95a7049b5eb2a0dea5eabf49e9ac109d36e58bf
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-03 16:48:08 +0200

    feat: implement discount by product id in discount service

--- src/main/kotlin/org/shangahi/sellio_backend/service/DiscountService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/DiscountService.kt
@@ -5,7 +5,9 @@ import org.shangahi.sellio_backend.api.dto.PageResponse
 import org.shangahi.sellio_backend.api.mapper.toPageResponse
 import org.shangahi.sellio_backend.api.mapper.toDiscountResponse
 import org.shangahi.sellio_backend.repository.DiscountRepository
+import org.shangahi.sellio_backend.repository.ProductRepository
 import org.shangahi.sellio_backend.repository.StoreRepository
+import org.shangahi.sellio_backend.service.exception.ProductNotFoundException
 import org.shangahi.sellio_backend.service.exception.StoreNotFoundException
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -14,6 +16,7 @@ import java.util.UUID
 @Service
 class DiscountService(
     private val discountRepository: DiscountRepository,
+    private val productRepository: ProductRepository,
     private val storeRepository: StoreRepository
 ) {
 
@@ -27,4 +30,16 @@ class DiscountService(
 
         return discountPage.toPageResponse { it.toDiscountResponse() }
     }
+
+    fun getDiscountsByProductId(productId: UUID, pageable: Pageable): PageResponse<DiscountResponse> {
+
+        if (!productRepository.existsById(productId)) {
+            throw ProductNotFoundException()
+        }
+
+        val discountPage = discountRepository.findByProductId(productId, pageable)
+
+        return discountPage.toPageResponse { it.toDiscountResponse() }
+    }
+
 }
