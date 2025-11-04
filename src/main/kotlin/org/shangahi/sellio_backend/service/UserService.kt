4a27dcfbdded3550127314ba58b2f38cbe6b11f3
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-04 20:18:56 +0300

    feat: add insertUser service function

--- src/main/kotlin/org/shangahi/sellio_backend/service/UserService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/UserService.kt
@@ -3,6 +3,7 @@ package org.shangahi.sellio_backend.service
 import org.shangahi.sellio_backend.entity.User
 import org.shangahi.sellio_backend.repository.UserRepository
 import org.shangahi.sellio_backend.service.exception.UserNotFoundException
+import org.shangahi.sellio_backend.service.exception.UserPhoneNumberAlreadyExistsException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -23,6 +24,13 @@ class UserService(
     fun userExists(userId: UUID): Boolean {
         return userRepository.existsById(userId)
     }
+    fun insertUser(user: User): User {
+        if (userRepository.findByPhoneNumber(user.phoneNumber) != null) {
+            throw UserPhoneNumberAlreadyExistsException()
+        }
+        return userRepository.save(user)
+    }
+
 
 
 }
