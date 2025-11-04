d6cf458737302d9ec21e66b58b7a3b2355cda603
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-04 20:21:23 +0300

    feat: add UserInsertRequest dto

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/dto/UserInsertRequest.kt
@@ -0,0 +1,12 @@
+package org.shangahi.sellio_backend.api.dto
+
+data class UserInsertRequest(
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String?,
+    val city: String,
+    val country: String,
+    val password: String,
+    val avatarUrl: String?
+)
