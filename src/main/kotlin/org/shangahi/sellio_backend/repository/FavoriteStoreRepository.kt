94221e4c6de5b25bc438aae0af7ef31a0c00e5f0
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-05 08:09:05 +0300

    feat: add existsByUserAndStore in FavoriteStoreRepository

--- src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteStoreRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteStoreRepository.kt
@@ -1,6 +1,8 @@
 package org.shangahi.sellio_backend.repository
 
 import org.shangahi.sellio_backend.entity.FavoriteStore
+import org.shangahi.sellio_backend.entity.Store
+import org.shangahi.sellio_backend.entity.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -9,4 +11,5 @@ import java.util.*
 interface FavoriteStoreRepository : JpaRepository<FavoriteStore, UUID> {
 
     fun findByUserId(userId: UUID, pageable: Pageable): Page<FavoriteStore>
+    fun existsByUserAndStore(user: User, store: Store): Boolean
 }
