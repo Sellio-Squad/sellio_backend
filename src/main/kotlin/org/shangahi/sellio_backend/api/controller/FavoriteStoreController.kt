93a22f510d185aaa7c0e6cd6e3364b5b1ecf852a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-05 13:29:28 +0300

    feat: add addFavoriteStore endpoint in FavoriteStoreController

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteStoreController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteStoreController.kt
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,7 +20,13 @@ import java.util.UUID
 class FavoriteStoreController(
     private val favoriteStoreService: FavoriteStoreService
 ) {
-
+    @PostMapping("/{userId}/favorite-stores/{storeId}")
+    fun addFavoriteStore(
+        @PathVariable userId: UUID,
+        @PathVariable storeId: UUID
+    ): FavoriteStoreResponse {
+        return favoriteStoreService.addFavoriteStore(userId, storeId).toResponse()
+    }
     @GetMapping("/{userId}/favorite-stores")
     fun getFavoriteStoresByUserId(
         @PathVariable userId: UUID,
