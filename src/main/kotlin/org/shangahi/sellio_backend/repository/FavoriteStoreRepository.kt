64b9e97c49f5746e3dbf85a94c482f0036702a8a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-05 13:34:16 +0300

    Revert "feat: add existsByUserAndStore in FavoriteStoreRepository"
    This reverts commit 94221e4c6de5b25bc438aae0af7ef31a0c00e5f0.

--- src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteStoreRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteStoreRepository.kt
@@ -1,8 +1,6 @@
 package org.shangahi.sellio_backend.repository
 
 import org.shangahi.sellio_backend.entity.FavoriteStore
-import org.shangahi.sellio_backend.entity.Store
-import org.shangahi.sellio_backend.entity.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -11,5 +9,4 @@ import java.util.*
 interface FavoriteStoreRepository : JpaRepository<FavoriteStore, UUID> {
 
     fun findByUserId(userId: UUID, pageable: Pageable): Page<FavoriteStore>
-    fun existsByUserAndStore(user: User, store: Store): Boolean
 }
