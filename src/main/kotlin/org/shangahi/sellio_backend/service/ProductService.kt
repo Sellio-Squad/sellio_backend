f803851a35b3ad62f2db34efcda3e38f6e59dc9b
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-05 08:35:00 +0200

    feature: add search by title to service

--- src/main/kotlin/org/shangahi/sellio_backend/service/ProductService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/ProductService.kt
@@ -1,10 +1,14 @@
 package org.shangahi.sellio_backend.service
 
+import org.shangahi.sellio_backend.api.dto.response.PageResponse
 import org.shangahi.sellio_backend.api.dto.response.ProductCardResponse
+import org.shangahi.sellio_backend.api.mapper.toPageResponse
 import org.shangahi.sellio_backend.api.mapper.toProductCardResponse
 import org.shangahi.sellio_backend.repository.ProductRepository
+import org.shangahi.sellio_backend.service.exception.ProductNotFoundException
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -22,5 +26,16 @@ class ProductService(
         val productPage = productRepository.findAllByStoreId(storeId, pageable)
 
         return productPage.map { product -> product.toProductCardResponse() }
+    }
+
+    fun searchProductsByTitle(title: String, pageable: Pageable): PageResponse<ProductCardResponse> {
+
+        if (title.isBlank()) {
+            throw ProductNotFoundException()
+        }
+
+        val productPage = productRepository.findByTitleContainingIgnoreCase(title, pageable)
+
+        return productPage.toPageResponse{it.toProductCardResponse()}
     }
 }
