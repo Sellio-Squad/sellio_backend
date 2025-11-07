5f46219320e1ba9d947fc3665e57701b7f427d9f
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-07 16:51:13 +0200

    feat: add FavoriteStoresDoc annotations

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteStoreController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteStoreController.kt
@@ -1,10 +1,12 @@
 package org.shangahi.sellio_backend.api.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.shangahi.sellio_backend.api.dto.request.FavouriteStoreRequest
 import org.shangahi.sellio_backend.api.dto.response.FavoriteStoreResponse
 import org.shangahi.sellio_backend.api.dto.response.PageResponse
 import org.shangahi.sellio_backend.api.mapper.toPageResponse
 import org.shangahi.sellio_backend.api.mapper.toResponse
+import org.shangahi.sellio_backend.api.swagger.FavoriteStoresDocs
 import org.shangahi.sellio_backend.service.FavoriteStoreService
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -17,17 +19,21 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
-@RequestMapping("/v1/stores")
+@RequestMapping("/v1/favorite-stores")
+@Tag(name = "Favorite Stores", description = "Endpoints for managing favorite stores")
 class FavoriteStoreController(
     private val favoriteStoreService: FavoriteStoreService
 ) {
+    @FavoriteStoresDocs.ToggleFavoriteStores
     @PostMapping("/toggle")
     fun toggleFavoriteStore(
         @RequestBody request: FavouriteStoreRequest
     ): FavoriteStoreResponse {
         return favoriteStoreService.toggleFavoriteStore(request).toResponse()
     }
-    @GetMapping("/{userId}/favorite-stores")
+
+    @FavoriteStoresDocs.GetFavoriteStores
+    @GetMapping("/{userId}")
     fun getFavoriteStoresByUserId(
         @PathVariable userId: UUID,
         @PageableDefault(page = 0, size = 10) pageable: Pageable
