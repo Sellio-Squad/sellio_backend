301cec2b21f103cdeb60494a1cfa70ea2d3ab804
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-07 13:30:27 +0200

    feat: add needed annotations for controller

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteProductController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/FavoriteProductController.kt
@@ -1,37 +1,36 @@
 package org.shangahi.sellio_backend.api.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.shangahi.sellio_backend.api.dto.FavoriteToggleRequest
 import org.shangahi.sellio_backend.api.dto.response.FavoriteProductsResponse
+import org.shangahi.sellio_backend.api.swagger.FavoriteProductDocs
 import org.shangahi.sellio_backend.service.FavoriteProductService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
-
-// http://localhost:8080/v1/favorite-products/userId
-// http://localhost:8080/v1/favorite-products/toggle
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/v1/favorite-products")
-class FavoriteProductController(private val favoriteProductService: FavoriteProductService) {
+@Tag(name = "Favorite Products", description = "Endpoints for managing user favorite products")
+class FavoriteProductController(
+    private val favoriteProductService: FavoriteProductService
+) {
 
+    @FavoriteProductDocs.GetFavorites
     @GetMapping("/{userId}")
-    fun findByUserId(@PathVariable userId: UUID): List<FavoriteProductsResponse> {
-
+    fun findByUserId(
+        @PathVariable userId: UUID
+    ): List<FavoriteProductsResponse> {
         return favoriteProductService.getFavoriteProductsByUserId(userId)
     }
 
+
+    @FavoriteProductDocs.ToggleFavorite
     @PostMapping("/toggle")
-
-    fun toggleFavorite(@RequestBody request: FavoriteToggleRequest): ResponseEntity<String> {
-
+    fun toggleFavorite(
+        @RequestBody request: FavoriteToggleRequest
+    ): ResponseEntity<String> {
         val message = favoriteProductService.toggleFavorite(request)
-
         return ResponseEntity.ok(message)
     }
-
 }
