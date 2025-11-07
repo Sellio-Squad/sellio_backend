fa1a8605ef506d9df20a0493fae297242915e4e2
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-07 18:35:58 +0200

    refactor: store rating service

--- src/main/kotlin/org/shangahi/sellio_backend/service/StoreRatingService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/StoreRatingService.kt
@@ -2,15 +2,23 @@ package org.shangahi.sellio_backend.service
 
 import org.shangahi.sellio_backend.api.dto.response.StoreRatingResponse
 import org.shangahi.sellio_backend.repository.StoreRatingRepository
+import org.shangahi.sellio_backend.repository.StoreRepository
+import org.shangahi.sellio_backend.service.exception.StoreNotFoundException
 import org.springframework.stereotype.Service
 import java.util.UUID
 import kotlin.collections.forEach
 
 
 @Service
-class StoreRatingService(private val storeRatingRepository: StoreRatingRepository) {
+class StoreRatingService(
+    private val storeRatingRepository: StoreRatingRepository,
+    private val storeRepository: StoreRepository
+) {
     fun getStoreRatingSummary(storeId: UUID): StoreRatingResponse {
 
+        if (!storeRepository.existsById(storeId)) {
+            throw StoreNotFoundException()
+        }
         val stats = storeRatingRepository.getRatingStats(storeId)
         val categorizedRatings = storeRatingRepository.getCategorizedRatings(storeId)
         val categorizedRatingsMap = mutableMapOf(
