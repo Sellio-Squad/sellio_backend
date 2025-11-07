e2b620ba07fdbdbeeb5c82dc42110401bebde734
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-07 13:26:37 +0200

    feat: add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ java {
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -27,6 +26,7 @@ dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-actuator")
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation ("org.flywaydb:flyway-core")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
     runtimeOnly ("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
