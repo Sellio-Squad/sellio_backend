f006429d0cf8c6e0c48c28bf0581e41f3c2b9e09
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-07 18:36:44 +0200

    feat: add store rating doc

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/swagger/StoreRatingDoc.kt
@@ -0,0 +1,82 @@
+package org.shangahi.sellio_backend.api.swagger
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.shangahi.sellio_backend.api.dto.response.ErrorResponse
+import org.shangahi.sellio_backend.api.dto.response.StoreRatingResponse
+
+annotation class StoreRatingDoc {
+    @Operation(
+        summary = "Get Store Rating",
+        description = "Retrieve a specific Store rating info by storeId",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Rating retrieved successfully",
+
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = StoreRatingResponse::class),
+                        examples = [
+                            ExampleObject(
+                                name = "Rating info",
+                                value = """
+                        {
+                            "id": "57a212fc-e4ac-4f70-90cd-21f95dc600ba",
+                            "averageRating": 4.0,
+                            "totalRatings": 2,
+                            "ratingCategorize": {
+                                "5": 1,
+                                "4": 0,
+                                "3": 1,
+                                "2": 0,
+                                "1": 0
+                            }
+                        }
+                        """
+                            )
+                        ],
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Store not found",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponse::class),
+                        examples = [
+                            ExampleObject(
+                                name = "StoreNotFoundExample",
+                                value = ErrorResponseExample.STORE_NOT_FOUND
+                            )
+                        ]
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ErrorResponse::class),
+                        examples = [
+                            ExampleObject(
+                                name = "InternalServerErrorExample",
+                                value = ErrorResponseExample.INTERNAL_SERVER_ERROR
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+    annotation class GetRatingInfo
+
+}
