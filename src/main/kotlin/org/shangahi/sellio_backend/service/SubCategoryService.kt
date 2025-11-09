19ba9f70eab23080725d70a36d9c28bcf5987c66
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-10 01:05:22 +0200

    feat: throw exceptions into subcategory service

--- src/main/kotlin/org/shangahi/sellio_backend/service/SubCategoryService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/SubCategoryService.kt
@@ -9,6 +9,7 @@ import org.shangahi.sellio_backend.repository.StoreRepository
 import org.shangahi.sellio_backend.repository.SubCategoryRepository
 import org.shangahi.sellio_backend.service.exception.CategoryNotFoundException
 import org.shangahi.sellio_backend.service.exception.StoreNotFoundException
+import org.shangahi.sellio_backend.service.exception.SubCategoryAlreadyExistException
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -36,7 +37,10 @@ class SubCategoryService(
 
     fun create(request: SubCategoryRequest): SubCategoryResponse {
         val category = categoryRepository.findById(request.categoryId)
-            .orElseThrow { RuntimeException("Category not found with id ${request.categoryId}") }
+            .orElseThrow { CategoryNotFoundException() }
+        if (subCategoryRepository.existsByTitle(request.title)) {
+            throw SubCategoryAlreadyExistException()
+        }
         val saved = subCategoryRepository.save(request.toEntity(category))
         return saved.toResponse()
     }
