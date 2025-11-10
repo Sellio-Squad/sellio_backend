1377abbdd3d6688d5b9caefd02df60c8b08136c1
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-10 15:11:34 +0300

    feat: add SecurityConfig

---
+++ src/main/kotlin/org/shangahi/sellio_backend/security/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package org.shangahi.sellio_backend.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf { csrf ->
+                csrf.disable()
+            }.sessionManagement { sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }.build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+}
