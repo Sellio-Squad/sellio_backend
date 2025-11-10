3f45308b9c3b99bb7d8b15e97ff9b2f68b530416
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-10 02:46:40 +0200

    feat: add discount doc annotation for discount controller

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/DiscountController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/DiscountController.kt
@@ -3,6 +3,7 @@ package org.shangahi.sellio_backend.api.controller
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.shangahi.sellio_backend.api.dto.response.DiscountResponse
 import org.shangahi.sellio_backend.api.dto.response.PageResponse
+import org.shangahi.sellio_backend.api.swagger.doc.DiscountDoc
 import org.shangahi.sellio_backend.service.DiscountService
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -14,11 +15,13 @@ import java.util.*
 
 @RestController
 @RequestMapping("/v1/discounts")
-@Tag(name = "Discount", description = "Endpoints for managing Discounts")
+@Tag(name = "Discount", description = "Endpoints for managing Discounts," +
+        " Retrieve Discount info info by storeId, categoryId, subCategoryId and productId")
 class DiscountController(
     private val discountService: DiscountService
 ) {
 
+    @DiscountDoc.GetDiscountByStoreId
     @GetMapping("/store/{storeId}")
     fun getStoreDiscounts(
         @PathVariable("storeId") storeId: UUID,
@@ -27,6 +30,7 @@ class DiscountController(
         return discountService.getDiscountsByStoreId(storeId, pageable)
     }
 
+    @DiscountDoc.GetDiscountByProductId
     @GetMapping("/product/{productId}")
     fun getProductDiscounts(
         @PathVariable("productId") productId: UUID,
@@ -35,6 +39,7 @@ class DiscountController(
         return discountService.getDiscountsByProductId(productId, pageable)
     }
 
+    @DiscountDoc.GetDiscountByCategoryId
     @GetMapping("/category/{CategoryId}")
     fun getCategoryDiscounts(
         @PathVariable("CategoryId") categoryId: UUID,
@@ -43,6 +48,7 @@ class DiscountController(
         return discountService.getDiscountsByCategoryId(categoryId, pageable)
     }
 
+    @DiscountDoc.GetDiscountBySubCategoryId
     @GetMapping("/sub-category/{subCategoryId}")
     fun getSubCategoryDiscounts(
         @PathVariable("subCategoryId") subCategoryId: UUID,
