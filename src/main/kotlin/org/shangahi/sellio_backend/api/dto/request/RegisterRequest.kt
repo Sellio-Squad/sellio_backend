3af479d31fe9ab9d365785530e6f6db696e7bd4e
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-11 17:24:50 +0300

    feat: add RegisterRequest

---
+++ src/main/kotlin/org/shangahi/sellio_backend/api/dto/request/RegisterRequest.kt
@@ -0,0 +1,16 @@
+package org.shangahi.sellio_backend.api.dto.request
+
+import java.util.UUID
+
+data class RegisterRequest(
+    val firstName: String,
+    val lastName: String,
+    val username: String,
+    val phoneNumber: String,
+    val password: String,
+    val city: String,
+    val country: String,
+    val avatarUrl: String?,
+    val email: String?,
+    val sessionId: UUID?
+)
