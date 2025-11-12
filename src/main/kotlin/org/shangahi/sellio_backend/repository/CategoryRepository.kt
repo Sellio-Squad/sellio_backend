4308ddb349df2323dab616b70591b3a62418da0b
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-13 01:33:40 +0200

    feat : add new query to get categories by storeId

--- src/main/kotlin/org/shangahi/sellio_backend/repository/CategoryRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/CategoryRepository.kt
@@ -2,8 +2,18 @@ package org.shangahi.sellio_backend.repository
 
 import org.shangahi.sellio_backend.entity.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.*
 
 interface CategoryRepository : JpaRepository<Category, UUID>{
     fun existsByTitle(name: String): Boolean
+    @Query("""
+        SELECT DISTINCT c FROM Category c
+        JOIN c.subCategories sc
+        JOIN sc.products psc
+        JOIN psc.product p
+        WHERE p.store.id = :storeId
+    """)
+    fun findCategoriesByStoreId(@Param("storeId") storeId: UUID): List<Category>
 }
