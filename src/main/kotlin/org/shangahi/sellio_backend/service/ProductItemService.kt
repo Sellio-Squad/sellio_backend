ccbaa9c8669ef29865e4394819daa3fbd055fbf7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-13 01:38:17 +0200

    feat : implement insertProductItem

--- src/main/kotlin/org/shangahi/sellio_backend/service/ProductItemService.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/service/ProductItemService.kt
@@ -1,15 +1,25 @@
 package org.shangahi.sellio_backend.service
 
+import org.shangahi.sellio_backend.api.dto.ProductItemRequest
+import org.shangahi.sellio_backend.entity.ProductItem
 import org.shangahi.sellio_backend.model.OrderStatus
 import org.shangahi.sellio_backend.model.TrendingProduct
-import org.shangahi.sellio_backend.repository.ProductItemRepository
+import org.shangahi.sellio_backend.repository.*
+import org.shangahi.sellio_backend.service.exception.ProductNotFoundException
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ProductItemService(
-    private val productItemRepository: ProductItemRepository
+    private val productItemRepository: ProductItemRepository,
+    private val productRepository: ProductRepository,
+    private val colorRepository: ColorRepository,
+    private val sizeRepository: SizeRepository,
+    private val weightRepository: WeightRepository,
+    private val discountRepository: DiscountRepository
 ) {
     fun getTrendingProducts(pageable: Pageable): Page<TrendingProduct> {
         val trendingProducts = productItemRepository.findTrendingProducts(OrderStatus.COMPLETED, pageable)
@@ -19,5 +29,36 @@ class ProductItemService(
         } else {
             trendingProducts
         }
+    }
+    fun insertProductItem(request: ProductItemRequest,productId: UUID): ProductItem {
+
+        val product = productRepository.findByIdOrNull(productId)
+            ?: throw ProductNotFoundException()
+
+        val color = request.colorId?.let {
+            colorRepository.findByIdOrNull(it)
+        }
+        val size = request.sizeId?.let {
+            sizeRepository.findByIdOrNull(it)
+        }
+        val weight = request.weightId?.let {
+            weightRepository.findByIdOrNull(it)
+        }
+
+        val discount = request.discountId?.let {
+            discountRepository.findByIdOrNull(it)
+        }
+        val productItem = ProductItem(
+            product = product,
+            price = request.price,
+            discount = discount,
+            color = color,
+            size = size,
+            weight = weight,
+            stock = request.stock,
+            variationImageUrl = request.variationImageUrl,
+        )
+
+        return productItemRepository.save(productItem)
     }
 }
