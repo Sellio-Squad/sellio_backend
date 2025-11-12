cfbaf658ad2daa241bf1d35d41ad689a2fd49a3b
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-12 23:48:15 +0300

    feat: implement OtpService

---
+++ src/main/kotlin/org/shangahi/sellio_backend/service/OtpService.kt
@@ -0,0 +1,65 @@
+package org.shangahi.sellio_backend.service
+
+import org.shangahi.sellio_backend.entity.OtpLog
+import org.shangahi.sellio_backend.repository.OtpLogRepository
+import org.shangahi.sellio_backend.security.service.otp.OtpGenerator
+import org.shangahi.sellio_backend.service.exception.InvalidOtpException
+import org.shangahi.sellio_backend.service.exception.OtpExpiredException
+import org.shangahi.sellio_backend.service.exception.UnauthorizedException
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.time.Instant
+import java.util.*
+
+@Service
+class OtpService(
+    private val otpGenerator: OtpGenerator,
+    private val otpLogRepository: OtpLogRepository
+) {
+    fun createOtp(phoneNumber: String): OtpLog {
+        val otp = otpGenerator.generateOtp()
+        val otpLog = OtpLog(
+            phoneNumber = phoneNumber,
+            otp = otp,
+            expireAt = Instant.now().plusSeconds(OTP_EXPIRY_SECONDS)
+        )
+        return otpLogRepository.save(otpLog)
+    }
+
+    fun verifyOtp(sessionId: UUID, otp: String) {
+        val otpLog = otpLogRepository.findByOtpAndSessionId(otp, sessionId)
+            ?: throw InvalidOtpException()
+        checkOtpExpiration(otpLog)
+        if (otpLog.isVerified)
+            throw InvalidOtpException()
+
+        otpLogRepository.verifyOtp(sessionId)
+    }
+
+    fun validateSessionVerified(sessionId: UUID) {
+        val otpLog = otpLogRepository.findLatestBySessionId(sessionId)
+            ?: throw UnauthorizedException()
+        checkOtpExpiration(otpLog)
+        if (!otpLog.isVerified) throw UnauthorizedException()
+    }
+
+    fun expireOtpBySessionId(sessionId: UUID) {
+        otpLogRepository.expireOtpBySessionId(sessionId)
+    }
+
+    private fun checkOtpExpiration(otpLog: OtpLog) {
+        if (otpLog.expireAt.isBefore(Instant.now()))
+            throw OtpExpiredException()
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    fun deleteExpiredOtps() {
+        otpLogRepository.deleteExpiredOtpsBefore(Instant.now())
+    }
+
+
+    companion object {
+        private const val OTP_EXPIRY_SECONDS = 4 * 60L
+    }
+}
