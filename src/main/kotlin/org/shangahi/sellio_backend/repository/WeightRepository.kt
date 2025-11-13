256cf988c5d562591f69ed6ae92961dcae64748c
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-13 04:13:59 +0200

    feat: add weight mapper and repo

--- src/main/kotlin/org/shangahi/sellio_backend/repository/WeightRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/WeightRepository.kt
@@ -3,4 +3,6 @@ package org.shangahi.sellio_backend.repository
 import org.shangahi.sellio_backend.entity.Weight
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface WeightRepository : JpaRepository<Weight, Int>
+interface WeightRepository : JpaRepository<Weight, Int>{
+    fun existsByValue(value: Double): Boolean
+}
