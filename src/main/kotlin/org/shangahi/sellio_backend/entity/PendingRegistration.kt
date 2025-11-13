d36b13eb3ae3ec8036706e48a56a663c3a6bd68a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-14 02:24:46 +0300

    feat: add PendingRegistration table

---
+++ src/main/kotlin/org/shangahi/sellio_backend/entity/PendingRegistration.kt
@@ -0,0 +1,39 @@
+package org.shangahi.sellio_backend.entity
+
+import jakarta.persistence.*
+import java.time.Instant
+import java.util.*
+
+@Entity
+@Table(name = "pending_registration")
+data class PendingRegistration(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "session_id", nullable = false)
+    val sessionId: UUID? = null,
+
+    @Column(name = "first_name", nullable = false)
+    val firstName: String,
+
+    @Column(name = "last_name", nullable = false)
+    val lastName: String,
+
+    @Column(name = "phone_number", nullable = false, unique = true)
+    val phoneNumber: String,
+
+    @Column(name = "password", nullable = false)
+    val password: String,
+
+    @Column(name = "email")
+    val email: String?,
+
+    @Column(name = "city", nullable = false)
+    val city: String,
+
+    @Column(name = "country", nullable = false)
+    val country: String,
+
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant = Instant.now()
+)
