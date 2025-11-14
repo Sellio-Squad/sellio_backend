a28c9aa113b8681faf855c048daa8fb6127c8e60
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-14 16:05:03 +0200

    feat: insert size doc annotations into controller

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/SizeController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/SizeController.kt
@@ -5,6 +5,7 @@ import jakarta.validation.Valid
 import org.shangahi.sellio_backend.api.dto.request.SizeRequest
 import org.shangahi.sellio_backend.api.dto.response.SizeResponse
 import org.shangahi.sellio_backend.api.mapper.toResponse
+import org.shangahi.sellio_backend.api.swagger.doc.SizeDoc
 import org.shangahi.sellio_backend.service.SizeService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -16,17 +17,19 @@ import java.util.*
 @RequestMapping("/v1/size")
 @Tag(name = "Sizes", description = "Endpoints for managing product sizes")
 class SizeController(private val sizeService: SizeService) {
+    @SizeDoc.GetAllSizes
     @GetMapping
     fun getAllSizes(): List<SizeResponse> {
         val response = sizeService.getAllSizes().map { it.toResponse() }
         return response
     }
 
+    @SizeDoc.GetSize
     @GetMapping("/{id}")
     fun getSizeById(@PathVariable id: Int): SizeResponse {
         return sizeService.getSizeById(id).toResponse()
     }
-
+    @SizeDoc.InsertSize
     @PostMapping
     fun createSize(@Valid @RequestBody request: SizeRequest): ResponseEntity<SizeResponse> {
         val size = sizeService.createSize(request)
@@ -34,12 +37,14 @@ class SizeController(private val sizeService: SizeService) {
         return ResponseEntity.created(location).body(size.toResponse())
     }
 
+    @SizeDoc.UpdateSize
     @PutMapping("/{id}")
     fun updateSize(@Valid @RequestBody request: SizeRequest, @PathVariable id: Int): ResponseEntity<SizeResponse> {
         val updateSize = sizeService.updateSize(id, request)
         return ResponseEntity.ok(updateSize.toResponse())
     }
 
+    @SizeDoc.DeleteSize
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteSize(@PathVariable id: Int): ResponseEntity<String> {
