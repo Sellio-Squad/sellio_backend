29c1594ccec48f17cd5f143a2adb81ba46a6dcc4
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-14 16:16:47 +0200

    feat: add color doc annotations into color controller

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/ColorController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/ColorController.kt
@@ -5,6 +5,7 @@ import jakarta.validation.Valid
 import org.shangahi.sellio_backend.api.dto.request.ColorRequest
 import org.shangahi.sellio_backend.api.dto.response.ColorResponse
 import org.shangahi.sellio_backend.api.mapper.toResponse
+import org.shangahi.sellio_backend.api.swagger.doc.ColorDoc
 import org.shangahi.sellio_backend.service.ColorService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -16,17 +17,20 @@ import java.util.*
 @RequestMapping("/v1/color")
 @Tag(name = "Colors", description = "Endpoints for managing product colors")
 class ColorController(private val colorService: ColorService) {
+    @ColorDoc.GetAllColors
     @GetMapping
     fun getAllColors(): List<ColorResponse> {
         val response = colorService.getAllColors().map { it.toResponse() }
         return response
     }
 
+    @ColorDoc.GetColor
     @GetMapping("/{id}")
     fun getColorById(@PathVariable id: Int): ColorResponse {
         return colorService.getColorById(id).toResponse()
     }
 
+    @ColorDoc.InsertColor
     @PostMapping
     fun createColor(@Valid @RequestBody request: ColorRequest): ResponseEntity<ColorResponse> {
         val color = colorService.createColor(request)
@@ -34,12 +38,14 @@ class ColorController(private val colorService: ColorService) {
         return ResponseEntity.created(location).body(color.toResponse())
     }
 
+    @ColorDoc.UpdateColor
     @PutMapping("/{id}")
     fun updateColor(@Valid @RequestBody request: ColorRequest, @PathVariable id: Int): ResponseEntity<ColorResponse> {
         val updateColor = colorService.updateColor(id, request)
         return ResponseEntity.ok(updateColor.toResponse())
     }
 
+    @ColorDoc.DeleteColor
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteColor(@PathVariable id: Int): ResponseEntity<String> {
