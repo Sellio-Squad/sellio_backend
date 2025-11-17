fcb42583fa6c0122b24b93ff761c0da6da1609e8
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-17 16:29:54 +0200

    feature: add findActiveStoreDiscounts into DiscountRepository

--- src/main/kotlin/org/shangahi/sellio_backend/repository/DiscountRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/DiscountRepository.kt
@@ -4,7 +4,10 @@ import org.shangahi.sellio_backend.entity.Discount
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.time.Instant
 import java.util.*
 
 @Repository
@@ -14,5 +17,16 @@ interface DiscountRepository : JpaRepository<Discount, UUID> {
     fun findByProductId(id: UUID, pageable: Pageable): Page<Discount>
     fun findBySubCategoryId(subCategoryId: UUID, pageable: Pageable): Page<Discount>
     fun findByCategoryId(subCategoryId: UUID, pageable: Pageable): Page<Discount>
-
+    @Query("""
+        SELECT d FROM Discount d
+        WHERE d.store.id = :storeId
+        AND d.product IS NULL
+        AND d.category IS NULL
+        AND d.subCategory IS NULL
+        AND (d.endDate IS NULL OR d.endDate >= :now)
+    """)
+    fun findActiveStoreDiscounts(
+        @Param("storeId") storeId: UUID,
+        @Param("now") now: Instant = Instant.now()
+    ): List<Discount>
 }
