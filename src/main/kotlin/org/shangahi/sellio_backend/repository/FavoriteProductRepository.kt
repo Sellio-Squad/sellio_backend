7aa23a58ddc62aebde36e0ed2a263024a6685f74
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-11-20 06:34:21 +0200

    feat: add fun deleteByProductId to FavoriteProductRepository

--- src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteProductRepository.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/FavoriteProductRepository.kt
@@ -5,15 +5,12 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Repository
 interface FavoriteProductRepository: JpaRepository<FavoriteProduct, UUID>{
     fun findByUserId(userId: UUID,pageable: Pageable): Page<FavoriteProduct>
     fun findByUserIdAndProductId(userId: UUID, productId: UUID): FavoriteProduct?
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun deleteByUserIdAndProductId(userId: UUID, productId: UUID)
+    fun deleteByProductId(productId: UUID)
 }
