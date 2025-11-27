2c8dc6a84611ab6be1f67ca7ae5e48fa4a4ce3a2
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-27 18:59:03 +0300

    feat: add mapper for store contacts creation and response

--- src/main/kotlin/org/shangahi/sellio_backend/api/mapper/StoreContactMapper.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/mapper/StoreContactMapper.kt
@@ -1,12 +1,32 @@
 package org.shangahi.sellio_backend.api.mapper
 
+import org.shangahi.sellio_backend.api.dto.request.StoreContactCreateRequest
 import org.shangahi.sellio_backend.api.dto.response.StoreContactResponse
+import org.shangahi.sellio_backend.api.dto.response.StoreCreationResponse
 import org.shangahi.sellio_backend.entity.StoreContact
+import org.shangahi.sellio_backend.model.StoreContactCreationModel
+import org.shangahi.sellio_backend.model.StoreContactModel
 
 fun StoreContact.toStoreContactResponse(): StoreContactResponse {
     return StoreContactResponse(
         id = this.id!!,
         storeId = this.store.id!!,
+        type = this.type,
+        value = this.value
+    )
+}
+
+fun StoreContactCreateRequest.toRequest(): StoreContactCreationModel {
+    return StoreContactCreationModel(
+        type = this.type,
+        value = this.value
+    )
+}
+
+fun StoreContactModel.toResponse(): StoreContactResponse {
+    return StoreContactResponse(
+        id = this.id,
+        storeId = this.storeId,
         type = this.type,
         value = this.value
     )
