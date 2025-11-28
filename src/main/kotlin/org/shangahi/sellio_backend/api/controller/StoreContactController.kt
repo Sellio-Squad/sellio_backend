5d3339e3451dd83f503f6d193fe421e8bd7c8c0a
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2025-11-28 11:25:17 +0300

    feat: add updateContact endpoint and change the path for other endpoints

--- src/main/kotlin/org/shangahi/sellio_backend/api/controller/StoreContactController.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/controller/StoreContactController.kt
@@ -4,38 +4,28 @@ import org.shangahi.sellio_backend.api.dto.request.StoreContactCreateRequest
 import org.shangahi.sellio_backend.api.dto.response.StoreContactResponse
 import org.shangahi.sellio_backend.api.mapper.toRequest
 import org.shangahi.sellio_backend.api.mapper.toResponse
+import org.shangahi.sellio_backend.api.mapper.toStoreContactResponse
 import org.shangahi.sellio_backend.service.StoreContactService
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/v1/store-contacts/{storeId}")
+@RequestMapping("/v1/store-contacts/")
 class StoreContactController(
     private val storeContactService: StoreContactService
 ) {
 
-    @PostMapping(name = "/add-contact")
-    fun addContact(
-        @PathVariable storeId: UUID,
-        @RequestBody request: StoreContactCreateRequest
-    ): StoreContactResponse {
-
-        val storeContactRequest = request.toRequest()
-        val response = storeContactService.addContact(storeId, storeContactRequest)
-        return response.toResponse()
-    }
-
-    @PostMapping("/add-multiple-contacts")
-    fun addMultipleContacts(
+    @PostMapping("{storeId}/add-contacts")
+    fun addContacts(
         @PathVariable storeId: UUID,
         @RequestBody requests: List<StoreContactCreateRequest>
     ): List<StoreContactResponse> {
         val storeContactRequests = requests.map { it.toRequest() }
-        val response = storeContactService.addMultipleContacts(storeId, storeContactRequests)
+        val response = storeContactService.addContacts(storeId, storeContactRequests)
         return response.map { it.toResponse() }
     }
 
-    @GetMapping(name = "/get-contacts")
+    @GetMapping( "{storeId}/get-contacts")
     fun getContacts(
         @PathVariable storeId: UUID
     ): List<StoreContactResponse> {
@@ -43,10 +33,18 @@ class StoreContactController(
         return response.map { it.toResponse() }
     }
 
+    @PutMapping("update-contact/{contactId}")
+    fun updateContact(
+        @PathVariable contactId: UUID,
+        @RequestBody request: StoreContactCreateRequest
+    ): StoreContactResponse {
+        val storeContactRequest = request.toRequest()
+        val response = storeContactService.updateContact(contactId, storeContactRequest)
+        return response.toStoreContactResponse()
+    }
 
-    @DeleteMapping("/{contactId}")
+    @DeleteMapping("delete-contact/{contactId}")
     fun deleteContact(
-        @PathVariable storeId: UUID,
         @PathVariable contactId: UUID
     ): String {
         storeContactService.deleteContact(contactId)
