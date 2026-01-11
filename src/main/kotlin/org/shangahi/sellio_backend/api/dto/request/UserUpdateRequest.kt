aad2270ebd451132ea338df12d7f4f80be67f9b0
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-11 03:26:44 +0200

    feature/update user update request

--- src/main/kotlin/org/shangahi/sellio_backend/api/dto/request/UserUpdateRequest.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/dto/request/UserUpdateRequest.kt
@@ -1,14 +1,18 @@
 package org.shangahi.sellio_backend.api.dto.request
 
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Size
 
 data class UserUpdateRequest(
+    @field:Size(min = 2, max = 50)
     val firstName: String?,
+    @field:Size(min = 2, max = 50)
     val lastName: String?,
-    val phoneNumber: String?,
     @field:Email(message = "Invalid email format")
+    @field:Size(min = 2, max = 100)
     val email: String?,
+    @field:Size(min = 2, max = 60)
     val city: String?,
+    @field:Size(min = 2, max = 60)
     val country: String?,
-    val avatarUrl: String?,
 )
