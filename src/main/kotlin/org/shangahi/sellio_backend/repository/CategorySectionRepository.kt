fac63edd6414939ac7ede24c842fc4227d9415f8
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-03 08:32:35 +0200

    feat: add category section repo

---
+++ src/main/kotlin/org/shangahi/sellio_backend/repository/CategorySectionRepository.kt
@@ -0,0 +1,11 @@
+package org.shangahi.sellio_backend.repository
+
+import org.shangahi.sellio_backend.entity.CategorySection
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface CategorySectionRepository : JpaRepository<CategorySection, UUID>{
+    fun findAllByIsActiveTrueOrderBySortOrderAsc(): List<CategorySection>
+}
