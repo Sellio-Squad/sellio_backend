43ef1deb63c97e727e99dd546b62a9c1883ad426
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-07 07:06:00 +0200

    feat: add image to response

--- src/main/kotlin/org/shangahi/sellio_backend/api/dto/response/CategoryResponse.kt
+++ src/main/kotlin/org/shangahi/sellio_backend/api/dto/response/CategoryResponse.kt
@@ -6,6 +6,7 @@ import java.util.*
 data class CategoryResponse(
     val id: UUID,
     val title: String,
+    val imageUrl: String?,
     val createdAt: Instant?,
     val updatedAt: Instant?,
     val subCategories: List<SubCategoryResponse> = emptyList()
